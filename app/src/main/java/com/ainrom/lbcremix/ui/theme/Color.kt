86c84f6174d85e31282e740cbda991fb522a6f8e
Romain <romain@yescapa.com>
2022-04-08 09:54:25 +0200

    squash test

--- app/src/main/java/com/ainrom/lbcremix/ui/theme/Color.kt
+++ app/src/main/java/com/ainrom/lbcremix/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.ainrom.lbcremix.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val colorPrimary = Color(0xff112B47)
+val colorPrimary = Color(0xFF4C6B8B)
 val colorPrimaryVariant = Color(0xFF49617B)
 val colorSecondary = Color(0xff69F0AE)
 val colorGrey = Color(0xffBDBDBD)
-val colorWhite = Color(0xFFFFFFFF)
+val colorWhite = Color(0xFFD82626)
 val colorBlack = Color(0xFF000000)
 val colorRed = Color(0xFFE82C2C)
