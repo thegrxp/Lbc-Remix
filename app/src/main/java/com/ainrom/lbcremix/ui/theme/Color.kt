83a599b51b0c381a3f2205147902537449bd824a
Romain <romain@yescapa.com>
2022-04-08 09:43:54 +0200

    Change color 1

--- app/src/main/java/com/ainrom/lbcremix/ui/theme/Color.kt
+++ app/src/main/java/com/ainrom/lbcremix/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.ainrom.lbcremix.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val colorPrimary = Color(0xff112B47)
+val colorPrimary = Color(0xFF4C6B8B)
 val colorPrimaryVariant = Color(0xFF49617B)
 val colorSecondary = Color(0xff69F0AE)
 val colorGrey = Color(0xffBDBDBD)
